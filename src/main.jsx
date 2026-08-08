131472fdccd5eedb42db70346e234bfc2fbac3b1
godlaveetianjali123-del <godlaveetianjali123@gmail.com>
2026-08-08 17:28:44 +0530

    Fix provider import casing

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-import {cartProvider} from "./context/CartContext";
-import {searchprovider} from "./context/searchcontext";
+import { CartProvider } from "./context/CartContext";
+import { SearchProvider } from "./context/searchcontext";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
