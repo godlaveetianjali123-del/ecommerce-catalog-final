b563b56eacf932393f2c5817bd64f80600b6587b
godlaveetianjali123-del <godlaveetianjali123@gmail.com>
2026-08-08 11:06:42 +0530

    Fix product routes

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,95 +1,50 @@
 import express from "express";
 import Product from "../models/Product.js";
 
-
 const router = express.Router();
 
-
 // GET all products
-
 router.get("/", async (req, res) => {
-
   try {
-
     const products = await Product.find();
-
     res.json(products);
-
-
   } catch (error) {
-
     res.status(500).json({
-      message: error.message
+      message: error.message,
     });
-
   }
-
 });
-
-
 
 // GET single product
-
 router.get("/:id", async (req, res) => {
-
   try {
-
-    const product = await Product.findById(
-      req.params.id
-    );
-
+    const product = await Product.findById(req.params.id);
 
     if (!product) {
-
       return res.status(404).json({
-        message: "Product not found"
+        message: "Product not found",
       });
-
     }
 
-
     res.json(product);
-
-
   } catch (error) {
-
     res.status(500).json({
-      message: error.message
+      message: error.message,
     });
-
   }
-
 });
-
-
 
 // CREATE product
-
 router.post("/", async (req, res) => {
-
   try {
-
-
-    const product = await Product.create(
-      req.body
-    );
-
+    const product = await Product.create(req.body);
 
     res.status(201).json(product);
-
-
   } catch (error) {
-
-
     res.status(500).json({
-      message: error.message
+      message: error.message,
     });
-
-
   }
-
 });
-
-
 
 export default router;
